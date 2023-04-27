bb54354c7dbe62aaa042723a18f0c5fe6a3a1a06
Gabe Salvania <bsalvania@ucsed.edu>
2023-04-26 20:49:29 -0700

    Did Q17

--- test.js
+++ test.js
@@ -1,20 +1,13 @@
-
-let statistics = {
-    redCars: 21,
-    blueCars: 45,
-    greenCars: 12,
-    raceCars: 5,
-    blackCars: 40,
-    rareCars: 2,
-};
-
-for (let car in statistics) {
-    if (car[0] == 'r' || statistics[car] % 2 == 1) {
-        console.log(statistics[car]);
+function modifyArray(array, callback) {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(callback(array[i]));
     }
-    // if (statistics[car] % 2 == 1) {
-    //     console.log(statistics[car]);
-    // }
-    // console.log(car[0]);
+    return newArr;
 }
-// console.log(statistics.redCars);
+
+function doSomething(num) {
+    return num * 2;
+}
+
+console.log(modifyArray([1,2,3], doSomething));
